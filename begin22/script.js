0a1149905624b52497549aa398aabefca86c57d0
new <gaybullayevamrayhona718@gmail.com>
2024-08-25 17:42:23 +0500

    begin22

---
+++ begin22/script.js
@@ -0,0 +1,8 @@
+function solve() {
+    let A = prompt("A sonni kiriting")
+    let B = prompt("B sonni kiriting")
+
+    if (A != null && B != null) {
+        document.getElementById("demo").innerHTML = "alishgan qiymat A: " + B + " B: " + A
+    }
+}
